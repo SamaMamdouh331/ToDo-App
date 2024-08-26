689382271867d1ecae07fd146c09e9af312a8ec5
mfarag <mfarag@ntgclarity.com>
2024-08-26 17:01:03 +0300

    finish splach activity.

--- app/src/main/java/com/example/todoapp/SplachActivity.kt
+++ app/src/main/java/com/example/todoapp/SplachActivity.kt
@@ -1,6 +1,8 @@
 package com.example.todoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,11 @@ import androidx.core.view.WindowInsetsCompat
 class SplachActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splach)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler(mainLooper).postDelayed({
+            val intent=Intent(this,ToDosActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
