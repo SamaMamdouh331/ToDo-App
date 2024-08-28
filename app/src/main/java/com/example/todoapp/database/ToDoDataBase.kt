adc4fc5208d999c3af36566ba64587c63996711a
mfarag <mfarag@ntgclarity.com>
2024-08-28 21:50:13 +0300

    created objesct from the database.

--- app/src/main/java/com/example/todoapp/database/ToDoDataBase.kt
+++ app/src/main/java/com/example/todoapp/database/ToDoDataBase.kt
@@ -1,10 +1,21 @@
 package com.example.todoapp.database
 
+import android.content.Context
 import androidx.room.Database
 
 @Database(entities = [ToDoEntity::class], version = 1)
 abstract class ToDoDataBase {
+    Companion
+    object {
+        private var toDoDataBase: ToDoDataBase? = null
 
-    abstract fun getToDo(): ToDodao
-
+        fun getInstance(context: Context): ToDoDataBase {
+            if (ToDoDataBase.`<no name provided>`.toDoDataBase == null) {
+                Room.databaseBuilder(
+                    this,
+                    ToDoDataBase::class.java, "todos"
+                ).build()
+            }
+        }
+    }
 }
